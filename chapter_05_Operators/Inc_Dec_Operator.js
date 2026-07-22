2fe79a44e37da54edf44abb28f6987e771bdd349
Harshitha M <harshitha@example.com>
2026-07-22 11:24:47 +0530

    Add JavaScript learning exercises

---
+++ chapter_05_Operators/Inc_Dec_Operator.js
@@ -0,0 +1,24 @@
+/*let i=1;
+let result = i++ + ++i;
+console.log(result, i); */
+
+/*let a = 10;
+console.log(++a + ++a);
+console.log(a);*/
+
+/*let a = 100;
+let result= console.log(a++ + ++a +a++ + ++a);
+console.log(a);*/
+
+/*let a = 37;
+console.log(--a + a--);
+console.log(a);*/
+
+/*let a = 5; 
+let b = a-- - --a; 
+console.log(b, a);*/
+
+let i = 1; 
+let r = i++ > 1 ? i++ : ++i; 
+console.log(r, i);
+
---
+++ chapter_05_Operators/Ternary_Operator.js
@@ -0,0 +1,8 @@
+/*let actSC = 200;
+let expSC = 100;
+let tesRES = actSC === expSC ? "Test Passed" : "Test Failed";
+console.log("Test Result: " + tesRES);*/
+
+let a= 10;
+console.log(++a +a+ a++);
+console.log(a);
